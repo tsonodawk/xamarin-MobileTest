561987ba5a8be341b9db5964abf6eb28f9c41818
Toshiro Sonoda <toshiro.sonoda@gmail.com>
2019-09-09 18:27:23 +0900

    画面遷移

---
+++ MobileTest/MobileTest/CompanySelect.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace MobileTest
+{
+    public partial class CompanySelect : ContentPage
+    {
+        public CompanySelect()
+        {
+            InitializeComponent();
+        }
+
+        void OnClickToBack(object sender, EventArgs e)
+        {
+            // 前？画面に戻る
+            Navigation.PopAsync(true);
+        }
+
+    }
+}
